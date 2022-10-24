aaad993b1fa1a9c08304ac2539b1a81e6cf104bb
luongvuc0622i1 <vuhienluong.td151218@gmail.com>
2022-10-24 08:24:10 +0700

    update

--- src/main/java/com/codegym/controller/ExpendituresController.java
+++ src/main/java/com/codegym/controller/ExpendituresController.java
@@ -9,10 +9,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.FileCopyUtils;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -59,5 +56,48 @@ public class ExpendituresController {
         modelAndView.addObject("expendituresForm", expendituresForm);
         modelAndView.addObject("message", "Created new expenditures successfully !");
         return modelAndView;
+    }
+
+    @GetMapping("/edit-expenditures/{id}")
+    public ModelAndView showEditForm(@PathVariable Long id) {
+        Expenditures expenditures = expendituresService.findById(id);
+        if (expenditures != null) {
+            ModelAndView modelAndView = new ModelAndView("/edit");
+            modelAndView.addObject("expenditures", expenditures);
+            return modelAndView;
+
+        } else {
+            ModelAndView modelAndView = new ModelAndView("/error.404");
+            return modelAndView;
+        }
+    }
+
+    @PostMapping("/edit-expenditures")
+    public ModelAndView updateCustomer(@ModelAttribute("expenditures") Expenditures expenditures) {
+        expendituresService.save(expenditures);
+        ModelAndView modelAndView = new ModelAndView("/edit");
+        modelAndView.addObject("expenditures", expenditures);
+        modelAndView.addObject("message", "Expenditures updated successfully");
+        return modelAndView;
+    }
+
+    @GetMapping("/delete-expenditures/{id}")
+    public ModelAndView showDeleteForm(@PathVariable Long id) {
+        Expenditures expenditures = expendituresService.findById(id);
+        if (expenditures != null) {
+            ModelAndView modelAndView = new ModelAndView("/delete");
+            modelAndView.addObject("expenditures", expenditures);
+            return modelAndView;
+
+        } else {
+            ModelAndView modelAndView = new ModelAndView("/error.404");
+            return modelAndView;
+        }
+    }
+
+    @PostMapping("/delete-expenditures")
+    public String deleteCustomer(@ModelAttribute("expenditures") Expenditures expenditures) {
+        expendituresService.remove(expenditures.getExpenditures_id());
+        return "redirect:expenditures";
     }
 }
